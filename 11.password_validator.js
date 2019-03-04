a526d95abfba79db7343141c14c480a8bbaf2571
lentqq <lentqq@gmail.com>
2019-03-04 07:04:38 +0200

    I finished the file

--- 11.password_validator.js
+++ 11.password_validator.js
@@ -11,5 +11,29 @@ if (password.length < 6 || password.length > 10){
 
    for (let index in password){
        let asciiElement = password.charCodeAt(index);
+
+       if (asciiElement > 47 && asciiElement < 58){
+           digitCount ++;
+       }
+
+       if (asciiElement < 48 || (asciiElement > 57 && asciiElement < 97) || asciiElement > 122){
+           incorectElement = true;
+       }
+   }
+
+   if (incorectElement){
+       console.log('Password must consist only of letters and digits');
+       isValid = false;
+   }
+
+   if (digitCount < 2){
+       console.log('Password must have at least 2 digit');
+       isValid = false;
+   }
+
+   if (isValid){
+       console.log('Password is valid');
    }
 }
+
+passwordValidator('Pa$s$s')
