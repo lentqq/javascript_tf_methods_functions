819e41ae820ed157be9c57c8ce05635a2c5f249a
lentqq <lentqq@gmail.com>
2019-03-04 06:06:34 +0200

    I add the file

--- 11.password_validator.js
+++ 11.password_validator.js
@@ -1,3 +1,15 @@
-function passwordValidator(string){
-    
+function passwordValidator(password) {
+    let isValid = true;
+    let digitCount = 0;
+    let incorectElement = false;
+    password = password.toLowerCase();
+
+if (password.length < 6 || password.length > 10){
+    console.log('Password must be between 6 and 10 characters');
+    isValid = false;
+}
+
+   for (let index in password){
+       let asciiElement = password.charCodeAt(index);
+   }
 }
