bdc202a62cf9b6789bf4efce23f37a24d354579b
lentqq <lentqq@gmail.com>
2019-03-06 12:31:39 +0200

    Editing file

--- 12.strongest_and_longest_word.js
+++ 12.strongest_and_longest_word.js
@@ -1,11 +1,22 @@
 function shortestAndLongestWord(string) {
-    let arr = string.split(' ');
-    let lengthElements = [];
-    
-    for (let i = 0; i < arr.length; i ++){
-        lengthElements.push(arr[i].length);
+    let arr = string.split(/[,? ]/);
+    let longestWord =arr[0];
+    let shortestWord = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i].length < shortestWord.length) {
+        shortestWord = arr[i];
+        }
     }
-    console.log(Math.min(lengthElements.join(',')));
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i].length > longestWord.length) {
+        longestWord = arr[i];
+        }
+    }
+
+    console.log(`Longest -> ${longestWord}`);
+    console.log(`Shortest -> ${shortestWord}`);
 }
 
-shortestAndLongestWord('hello by kolyo ot michuryn')
+shortestAndLongestWord('Hello people, are you familiar with the terms of application at the software university?')
