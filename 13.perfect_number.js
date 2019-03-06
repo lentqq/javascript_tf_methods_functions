f84da0418b3ca16c81f74a24258f4aa94d1cf761
lentqq <lentqq@gmail.com>
2019-03-06 09:13:39 +0200

    Finishing files

--- 13.perfect_number.js
+++ 13.perfect_number.js
@@ -1,5 +1,24 @@
-function perfectNumber() {
+function perfectNumber(number) {
+   let arrayDevisor = [];
+   let sumDevisor = 0;
 
+   for (let i = 1; i < number; i++) {
+       if (number % i === 0) {
+           arrayDevisor.push(i);
+       }
+   }
+
+   for ( let i = 0; i < arrayDevisor.length; i++) {
+         sumDevisor += arrayDevisor[i];
+   }
+
+   if (sumDevisor === number) {
+       console.log('We have a perfect number!');
+   }
+    
+   else {
+       console.log('It`s not so perfect!');
+   }
 }
 
-perfectNumber()
+perfectNumber(1236498)
--- 14.loading_bar.js
+++ 14.loading_bar.js
@@ -1,5 +1,36 @@
-function loadingBar() {
+function loadingBar(number) {
 
+    if (number === 100) {
+        console.log('100% complete!');
+        console.log('[%%%%%%%%%%]');
+    }
+    else {
+        let percent = `${number}%`;
+        let arraySigns = [printPercent(number), printDots(number)];
+
+        console.log(percent, '[', arraySigns.join(''), ']');
+        console.log('Still loading...');
+    }
 }
 
-loadingBar()
+loadingBar(100)
+
+function printPercent(number) {
+    let str = '';
+
+    for (let i = 0; i < number / 10; i++) {
+        str += '%' + '';
+    }
+
+    return str;
+}
+
+function printDots(number) {
+    let str = '';
+
+    for (let i = 0; i < (100 - number) / 10; i++) {
+        str += '.' + '';
+    }
+
+    return str;
+}
